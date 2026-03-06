923681cf99f5cd61587073332cc954b80bb9856b
zeynab-sh <z.shabanli@icloud.com>
2026-03-06 10:26:30 +0400

    Compare rectangles with test

---
+++ Rectangle.java
@@ -0,0 +1,26 @@
+class Rectangle {
+    int width;
+    int height;
+
+    public Rectangle(int w, int h){
+        width = w;
+        height = h;
+    }
+
+    public boolean equals(Object obj){
+        Rectangle rect = (Rectangle) obj;
+        if(this.width == rect.width && this.height == rect.height){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        Rectangle r1 = new Rectangle(5,10);
+        Rectangle r2 = new Rectangle(15,10);
+        Rectangle r3 = new Rectangle(5,10);
+
+        System.out.println(r1.equals(r2));
+        System.out.println(r1.equals(r3));
+    }
+}
